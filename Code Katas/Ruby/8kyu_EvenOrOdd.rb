56d0422fb3b2c6a94b3e61ab7e18121c01e1509a
SheaParrott <alexandergotthis@gmail.com>
2019-02-16 17:56:42 -0500

    Warmup Code Kata Completed

---
+++ Code Katas/Ruby/8kyu_EvenOrOdd.rb
@@ -0,0 +1,13 @@
+# Create a function (or write a script in Shell) that 
+# takes an integer as an argument and returns 
+# "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(number)
+  number % 2 == 0 ? 'Even' : 'Odd'
+end
+
+# best practice from solutions
+
+def even_or_odd(number)
+  number.even? ? "Even" : "Odd"
+end
