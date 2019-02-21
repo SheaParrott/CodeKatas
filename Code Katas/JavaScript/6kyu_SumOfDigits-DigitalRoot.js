0f8ee5a8df0f1023084c156c4b19cad23fc84649
SheaParrott <alexandergotthis@gmail.com>
2019-02-20 20:48:44 -0500

    Code Kata Completed
    Learned of a new mathematical equation for solving this type of problem

---
+++ Code Katas/JavaScript/6kyu_SumOfDigits-DigitalRoot.js
@@ -0,0 +1,55 @@
+// In this kata, you must create a digital root function.
+
+// A digital root is the recursive sum of all the digits
+// in a number. Given n, take the sum of the digits of n.
+// If that value has more than one digit, continue reducing
+// in this way until a single-digit number is produced.
+// This is only applicable to the natural numbers.
+
+// Here's how it works:
+
+// digital_root(16)
+// => 1 + 6
+// => 7
+
+// digital_root(942)
+// => 9 + 4 + 2
+// => 15 ...
+// => 1 + 5
+// => 6
+
+// digital_root(132189)
+// => 1 + 3 + 2 + 1 + 8 + 9
+// => 24 ...
+// => 2 + 4
+// => 6
+
+// digital_root(493193)
+// => 4 + 9 + 3 + 1 + 9 + 3
+// => 29 ...
+// => 2 + 9
+// => 11 ...
+// => 1 + 1
+// => 2
+
+function digital_root(n) {
+  let array = n.toString().split('')
+  while (array.length > 1) {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + parseInt(array[i])
+    }
+    array = sum.toString().split('')
+  }
+  return parseInt(array[0])
+}
+
+// best practice from solutions:
+// learned a better mathematical equation for solving this problem.
+// very interesting taking the remainder and adding one works for
+// this. I will speak to some of my math focused peers on the deeper
+// logistics of this.
+
+function digital_root(n) {
+  return ((n - 1) % 9) + 1
+}
