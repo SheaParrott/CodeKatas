3811abc0fd443865e706ee855e89d0c12661fb18
SheaParrott <alexandergotthis@gmail.com>
2019-02-23 08:19:35 -0500

    Code Kata Completed
    learned now to convert a integer into a character through this ASCII conversion method

---
+++ Code Katas/Ruby/8kyu_GetCharacterFromASCIIValue.rb
@@ -0,0 +1,16 @@
+# Get character from ASCII Value
+
+# Write a function getChar/GetChar/get_char which takes a 
+# number and returns the corresponding ASCII char for that value.
+
+# Example:
+
+# getChar(65) # => 'A'
+# For ASCII table, you can refer to http://www.asciitable.com/
+
+def getChar(c)
+  c.chr
+end
+
+# learned now to convert a integer into a character through 
+# this ASCII conversion method
