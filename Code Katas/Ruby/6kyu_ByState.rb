e9d0799683f75009cc372e856d6fddcda1a7a492
SheaParrott <alexandergotthis@gmail.com>
2019-03-01 12:31:46 -0500

    Revisted Gavin Starks answer to this Code Kata. Added a breakdown and what I learned.

--- Code Katas/Ruby/6kyu_ByState.rb
+++ Code Katas/Ruby/6kyu_ByState.rb
@@ -105,6 +105,17 @@ def bystate(str)
 end
 
 # I ran this one by Gavin Stark from SDG. This is his answer. Wow!
+# I learned an use-case for group_by and the value of using a hash
+# to complete a task. I aspire to gain the knowledge and skill to 
+# produce and accomplish future code challenges in an eligant way
+# such as done below. I will revist this answer and kata to better
+# understand how I can accomplish my aspirations. 
+
+
+# “Success is like a snowball. It takes momentum to build and the 
+# more you roll it in the right direction the bigger it gets.”
+
+# -Steve Ferrante
 
 STATES = {
 'AZ' => 'Arizona',
@@ -115,15 +126,15 @@ STATES = {
 'OK' => 'Oklahoma',
 'PA' => 'Pennsylvania',
 'VA' => 'Virginia'
-}
+} # this is our lookup table 
 
 def bystate(str)
-  str.
-    split("\n").
-    group_by { |entry| entry[-2..-1] }.
-    sort_by { |abbreviation, entries| abbreviation }.
-    map { |abbreviation, entries| [STATES[abbreviation.upcase], entries] }.
-    to_h.
-    map { |state, entries| "#{state}\r\n#{entries.sort.map { |entry| "..... #{entry[0..-3]}#{state}" }.join("\r\n")}".delete(",") }.
-    join("\r\n ")
+  p str.
+    split("\n"). # Splitting up the string and converting this to an array at each new line 
+    group_by { |entry| entry[-2..-1] }. # turning the array to a hash where the key is the last two items from the string in the array
+    sort_by { |abbreviation, entries| abbreviation }. # Sorting the keys alphabetically 
+    map { |abbreviation, entries| [STATES[abbreviation.upcase], entries] }. # Replacing the key values to the state name by using the current key abbreviations and finding it in the STATES lookup table
+    to_h. # turning the arrays inside the array back to a hash. This is a way to organise the data in a more readable format. This is an optional step. The code executes whether this is done or not. ("array in arrays"; also heard this called a tuple. However, I believe this is more of a Python term)
+    map { |state, entries| "#{state}\r\n#{entries.sort.map { |entry| "..... #{entry[0..-3]}#{state}" }.join("\r\n")}".delete(",") }. # iterating through all the keys to create a string. This is taking the name of the key adding the new line. Then, iterating through each entry to the hash to finish the string. The second part of the string is using all the string minus where the abbreviation is listed and the abbreviating is being replaced with the state name provided from the key. Then we are joining the string together and adding a "\r\n". Then deleting all commas. At this point we have an array where all state locations and entries are grouped in one string 
+    join("\r\n ") # Joining the array into one string and adding "\r\n" at each point where the string ends. 
 end
