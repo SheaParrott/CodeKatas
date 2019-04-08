08a55f1a6542f463736c05d5367053192b2ab889
SheaParrott <alexandergotthis@gmail.com>
2019-04-07 22:55:28 -0400

    Completed Code Kata

---
+++ Code Katas/JavaScript/7kyu_RemoveAnchorFromURL.js
@@ -0,0 +1,20 @@
+// Complete the function/method so that it returns the url
+// with anything after the anchor (#) removed.
+
+// Examples:
+
+// // returns 'www.codewars.com'
+// removeUrlAnchor('www.codewars.com#about')
+
+// // returns 'www.codewars.com?page=1'
+// removeUrlAnchor('www.codewars.com?page=1')
+
+function removeUrlAnchor(url) {
+  return url.includes('#') ? url.slice(0, url.indexOf('#')) : url
+}
+
+// best practice from solutions
+
+function removeUrlAnchor(url) {
+  return url.split('#')[0]
+}
