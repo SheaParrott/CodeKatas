e8cd3521a2628a35575ef9d61b68e499c1612f17
SheaParrott <alexandergotthis@gmail.com>
2019-05-13 22:48:07 -0400

    Completed Code Kata

---
+++ Code Katas/JavaScript/8kyu_DoYouSpeak"English"?.js
@@ -0,0 +1,19 @@
+// Given a string of arbitrary length with any ascii characters. Write a
+// function to determine whether the string contains the whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct
+// but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English",
+// false for it does not.
+
+const spEng = sentence => (sentence.match(/english/i) ? true : false)
+
+// best practice from solutions. I learned a interesting javascript method
+// when using regex with this solution!
+
+function spEng(s) {
+  return /english/i.test(s)
+}
