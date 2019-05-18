e39c9295fd2026df8bed76404350dcf9ad7ef791
SheaParrott <alexandergotthis@gmail.com>
2019-05-17 23:37:39 -0400

    Completed Code kate

---
+++ Code Katas/JavaScript/8kyu_GetAsciiValueOfCharacter.js
@@ -0,0 +1,12 @@
+// get ascii value of character
+
+// write a function getASCII which inputs a character and returns the
+// corresponding ascii value for that character. Example : getASCII('A') => 65
+
+// for ASCII table, you can refer to http://www.asciitable.com/
+
+getASCII = c => c.charCodeAt(0)
+
+// best practice from solutions
+
+const getASCII = c => c.charCodeAt(0)
