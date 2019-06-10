784323e2d3f78a21f0770eb5d8f07d2e56a4c3db
SheaParrott <alexandergotthis@gmail.com>
2019-06-09 21:35:41 -0400

    Completed Code Kata

---
+++ Code Katas/JavaScript/7kyu_HowManyArguements.js
@@ -0,0 +1,12 @@
+// args_count(1, 2, 3) -> 3
+// args_count(1, 2, 3, 10) -> 4
+
+function args_count() {
+  return [...arguments].length
+}
+
+// Best practice from solutions
+
+function args_count() {
+  return arguments.length
+}
