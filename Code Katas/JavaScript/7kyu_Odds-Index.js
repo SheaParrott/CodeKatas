daa0e84bc00f6ebf0fc7e50c724c01ea7ea0be10
SheaParrott <alexandergotthis@gmail.com>
2019-06-13 21:25:11 -0400

    Completed Code Kata

---
+++ Code Katas/JavaScript/7kyu_Odds-Index.js
@@ -0,0 +1,12 @@
+// You are given an array with several "even" words, one "odd" word, and some numbers mixed in.
+
+// Determine if any of the numbers in the array is the index of the "odd" word. If so, return
+// true, otherwise false.
+
+const oddBall = arr => arr.includes(arr.indexOf('odd'))
+
+// best practice from solutions
+
+function oddBall(arr) {
+  return arr.includes(arr.indexOf('odd'))
+}
