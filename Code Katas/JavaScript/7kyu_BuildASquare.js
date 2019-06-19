05d0839a8a7f2838daee185ca38547ee615f475f
SheaParrott <alexandergotthis@gmail.com>
2019-06-18 20:21:10 -0400

    Completed Code Kata

---
+++ Code Katas/JavaScript/7kyu_BuildASquare.js
@@ -0,0 +1,26 @@
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 0 and 50.
+
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+
+// +++
+// +++
+// +++
+
+function generateShape(int) {
+  let string = ''
+  for (let index = 0; index < int; index++)
+    for (let i = 0; i < int; i++)
+      string +=
+        i + 1 !== int || (index + 1 === int && i + 1 == int) ? '+' : '+\n'
+  return string
+}
+
+// best practice from solutions
+
+function generateShape(n) {
+  return ('+'.repeat(n) + '\n').repeat(n).trim()
+}
+
+// wow ^^ :O
+// Okay, will play around with that method. If I have seen this it was a while back.
