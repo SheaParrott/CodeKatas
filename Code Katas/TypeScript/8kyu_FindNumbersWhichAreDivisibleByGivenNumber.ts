7840f39e205f9a8bc307b44df48f6da3da1b98e2
Shea Parrott <alexandergotthis@gmail.com>
2024-09-26 11:43:32 -0400

    Create 8kyu_FindNumbersWhichAreDivisibleByGivenNumber.ts

---
+++ Code Katas/TypeScript/8kyu_FindNumbersWhichAreDivisibleByGivenNumber.ts
@@ -0,0 +1,9 @@
+// 8 kyu Find numbers which are divisible by given number
+
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor.
+// First argument is an array of numbers and the second is the divisor.
+
+// Example(Input1, Input2 --> Output)
+// [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+export const divisibleBy = (n: number[], d: number): number[] => n.filter(num => num % d === 0);
